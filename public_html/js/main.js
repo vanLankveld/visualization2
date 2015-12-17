c4e8193436e31c5db654f4edf0f0421b7c2856ab
Guus van Lankveld <Guus@wlan-228098.nbw.tue.nl>
2015-12-17 16:51:20 +0100

    Kleuren gebaseerd op waarde in CSV (alleen de R-waarde)

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,6 @@
 //This is executed as soon as the page's html is loaded:
 var basic;
+var csv;
 
 $(document).ready(function () {
 
@@ -11,6 +12,34 @@ $(document).ready(function () {
     basic = new Datamap({
         element: document.getElementById("main-view")
     });
+
+    $.get('connectivity.csv', function (data) {
+        var fileContents = data;
+        csv = d3.csv.parse(fileContents);
+        inputColors(csv[21]);
+    });
+
 });
 
+function getColor(value) {
+    var val = value / 100;
 
+    var r = Math.floor(val * 255);
+    var rHex = r.toString(16);
+    return "#" + rHex + "0000";
+}
+
+function inputColors(row) {
+    var json = "{";
+    var entries = [];
+    for (var country in row) {
+        if (row.hasOwnProperty(country) && country !== "Year") {
+            entries.push("\""+country+"\":\"" + getColor(row[country]) + "\"");
+        }
+    }
+    json += entries.join(',');
+    json += "}";
+    console.log(json);
+    
+    basic.updateChoropleth(JSON.parse(json));
+}
