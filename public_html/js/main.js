318c61fa77f1d1de39434324027fa06ff144a459
FrancisHoogendijk <francis_hoogendijk@live.nl>
2016-01-04 22:14:34 +0100

    Added Slider and made it work

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -13,33 +13,56 @@ $(document).ready(function () {
         element: document.getElementById("main-view")
     });
 
+    // Create slider
+    $("#slider").slider({
+        value: 2000,
+        min: 1990,
+        max: 2011,
+        step: 1,
+        slide: function (event, ui) {
+            $("#year").val(ui.value);
+            inputColors(ui.value);
+        }
+    });
+    // Initialize value
+    $("#year").val($("#slider").slider("value"));
+    
+    // Load CSV and initialize colors
     $.get('connectivity.csv', function (data) {
         var fileContents = data;
         csv = d3.csv.parse(fileContents);
-        inputColors(csv[21]);
+        inputColors($("#slider").slider("value"));
     });
-
+    
 });
 
 function getColor(value) {
     var val = value / 100;
 
     var r = Math.floor(val * 255);
-    var rHex = r.toString(16);
+    var rHex = ("0"+r.toString(16)).substr(-2);
     return "#" + rHex + "0000";
 }
 
-function inputColors(row) {
+function inputColors(year) {
+    var index=0;
+    for(var row in csv){
+        if(csv[row]["Year"]===year.toString()){
+            index=row;
+        }
+    }
+    
+    var row = csv[index];
     var json = "{";
     var entries = [];
     for (var country in row) {
         if (row.hasOwnProperty(country) && country !== "Year") {
-            entries.push("\""+country+"\":\"" + getColor(row[country]) + "\"");
+            entries.push("\"" + country + "\":\"" + getColor(row[country]) + "\"");
         }
     }
     json += entries.join(',');
     json += "}";
-    console.log(json);
-    
+    //console.log(json);
+
     basic.updateChoropleth(JSON.parse(json));
 }
